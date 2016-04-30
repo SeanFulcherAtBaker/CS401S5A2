7a7c7b6f07c001d9b374a2dffed9b7f7beaddacb
Sean Fulcher <sfulch01@baker.edu>
2016-04-30 17:34:10 -0400

    Ch 6 Assignment

---
+++ DistinguishingViewsWithID/DistinguishingViewsWithID/DistinguishingViewsWithID/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DistinguishingViewsWithID
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new SimplestKeypadPage();
+        }
+
+       
+    }
+}
---
+++ DistinguishingViewsWithID/DistinguishingViewsWithID/DistinguishingViewsWithID/SimplestKeypadPage.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DistinguishingViewsWithID
+{
+    public class SimplestKeypadPage : ContentPage
+    {
+        Label displayLabel;
+        Button backspaceButton;
+        public SimplestKeypadPage()
+        {
+            // Creates a keypad. 
+            StackLayout mainStack = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            // First row is Label. 
+            displayLabel = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.End
+            };
+            mainStack.Children.Add(displayLabel);
+            // Second row is backspace Btn. 
+            backspaceButton = new Button
+            {
+                Text = "\u21E6",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                IsEnabled = false
+            };
+            backspaceButton.Clicked += OnBackspaceButtonClicked;
+            mainStack.Children.Add(backspaceButton);
+            // Creates buttons 1 to 9 and 0. 
+            StackLayout rowStack = null;
+            for (int num = 1; num <= 10; num++)
+            {
+                if ((num - 1) % 3 == 0)
+                {
+                    rowStack = new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal
+                    };
+                    mainStack.Children.Add(rowStack);
+                }
+                Button digitButton = new Button
+                {
+                    Text = (num % 10).ToString(),
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                    StyleId = (num % 10).ToString()
+                };
+                digitButton.Clicked += OnDigitButtonClicked;
+                // For the zero button, expands horizontally to fill width. 
+                if (num == 10)
+                {
+                    digitButton.HorizontalOptions = LayoutOptions.FillAndExpand;
+                }
+                rowStack.Children.Add(digitButton);
+            }
+            this.Content = mainStack;
+        }
+        void OnDigitButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            displayLabel.Text += (string)button.StyleId;
+            backspaceButton.IsEnabled = true;
+        }
+        void OnBackspaceButtonClicked(object sender, EventArgs args)
+        {
+            string text = displayLabel.Text;
+            displayLabel.Text = text.Substring(0, text.Length - 1);
+            backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
+        }
+           
+    }
+}
